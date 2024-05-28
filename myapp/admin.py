8cfa8da21907ddc19e56446bfb029d0aabce1398
Hayden Cordeiro <cordeirohayden@gmail.com>
2024-05-28 13:53:34 -0400

    updated admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
+from  .models import Publisher, Book, Member, Order
 
-# Register your models here.
-from django.contrib import admin
-from .models import Publisher, Book
 
 # Register your models here.
 admin.site.register(Publisher)
 admin.site.register(Book)
+admin.site.register(Member)
+admin.site.register(Order)
