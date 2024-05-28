69c4eef5c679f5d971741a59468bc40c2111a763
Hayden Cordeiro <cordeirohayden@gmail.com>
2024-05-28 13:52:10 -0400

    Did part 1 of lab 4

--- myapp/models.py
+++ myapp/models.py
@@ -8,6 +8,7 @@ class Publisher(models.Model):
     name = models.CharField(max_length=200)
     website = models.URLField()
     city = models.CharField(max_length=20, blank=True)
+    country = models.CharField(max_length=50, default='USA')
 
 class Book(models.Model):
     CATEGORY_CHOICES = [
@@ -22,3 +23,30 @@ class Book(models.Model):
     num_pages = models.PositiveIntegerField(default=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     publisher = models.ForeignKey(Publisher, related_name='books', on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+
+# Lab4
+class Member(User):
+    STATUS_CHOICES = [
+        (1, 'Regular member'),
+        (2, 'Premium Member'),
+        (3, 'Guest Member'),
+    ]
+
+    status = models.IntegerField(choices=STATUS_CHOICES, default=1)
+    address = models.CharField(max_length=300, blank=True, null=True)
+    city = models.CharField(max_length=20, default='Windsor')
+    province=models.CharField(max_length=2, default='ON')
+    last_renewal = models.DateField(default=timezone.now)
+    auto_renew = models.BooleanField(default=True)
+    borrowed_books = models.ManyToManyField(Book, blank=True)
+
+class Order(models.Model):
+    ORDER_VALUE_CHOICES = [
+        (0, 'Purchase'),
+        (1, 'Borrow'),
+    ]
+    books = models.ManyToManyField(Book)
+    member = models.ForeignKey(Member, related_name='orders', on_delete=models.CASCADE)
+    order_type = models.IntegerField(choices=ORDER_VALUE_CHOICES, default=1)
+    order_date = models.DateField(default=timezone.now)
