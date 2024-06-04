9ebc188e5b3a8988552bbdacd8de674d7118c306
Hayden Cordeiro <cordeirohayden@gmail.com>
2024-06-04 12:24:58 -0400

    updated index and about

---
+++ myapp/urls1.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from myapp import views1
+
+app_name = 'myapp'
+
+urlpatterns = [
+    path(r'', views1.index, name='index'),
+    path(r'about', views1.about, name='about'),
+    ]
---
+++ myapp/views1.py
@@ -0,0 +1,21 @@
+# Import necessary classes
+from django.http import HttpResponse
+from .models import Publisher, Book, Member, Order
+
+# Create your views here.
+def index(request):
+    response = HttpResponse()
+    booklist = Book.objects.all().order_by('pk','-publisher__city')
+    heading1 = '<p>' + 'List of available books: ' + '</p>'
+    response.write(heading1)
+    for book in booklist:
+        para = '<p>'+ str(book.id) + ': ' + str(book)+ ': ' + str(book.publisher.name)+ ': ' + str(book.publisher.city) + '</p>'
+        response.write(para)
+    return response
+
+
+def about(request):
+    response = HttpResponse()
+    response.write('<p> This is an eBook APP. </p>')
+    return response
+
