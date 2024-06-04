a97a99396fc66fb07f4ed8369e9f479f35191549
Hayden Cordeiro <cordeirohayden@gmail.com>
2024-06-04 13:24:52 -0400

    fixed publisher

--- myapp/views1.py
+++ myapp/views1.py
@@ -5,11 +5,18 @@ from .models import Publisher, Book, Member, Order
 # Create your views here.
 def index(request):
     response = HttpResponse()
-    booklist = Book.objects.all().order_by('pk','-publisher__city')
+    response.write("Books <br>")
+    booklist = Book.objects.all().order_by('pk')
     heading1 = '<p>' + 'List of available books: ' + '</p>'
     response.write(heading1)
     for book in booklist:
-        para = '<p>'+ str(book.id) + ': ' + str(book)+ ': ' + str(book.publisher.name)+ ': ' + str(book.publisher.city) + '</p>'
+        para = '<p>'+ str(book.id) + ': ' + str(book) + ': ' +  str(book.title) + '</p>'
+        response.write(para)
+    response.write("<br>")
+    response.write("Publishers <br>")
+    publisherList = Publisher.objects.all().order_by('-city')
+    for publisher in publisherList:
+        para = '<p>' + str(publisher.name)+ ': ' + str(publisher.city) + '</p>'
         response.write(para)
     return response
 
