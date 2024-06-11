41f23e364706d4b23465248fc6eeab49e490e8a7
Hayden Cordeiro <cordeirohayden@gmail.com>
2024-06-11 13:04:56 -0400

    Completed till 4. Create about0.html.

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+from myapp.models import Book
+
+
+def index(request):
+    booklist = Book.objects.all().order_by('id')[:10]
+    return render(request, 'myapp/index0.html', {'booklist': booklist})
+
+def about(request):
+    return render(request, 'myapp/about0.html')
--- mysiteS24/urls.py
+++ mysiteS24/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+app_name = 'myapp'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-path(r'myapp/', include('myapp.urls1')),
+    # path(r'myapp/', include('myapp.urls1')),
+    path(r'myapp/', include('myapp.urls')),
+
 ]
