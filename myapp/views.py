15dd231e49c77a268a5705e5ce8490ce0a25d07a
diyonizaveri <diyonizaveri@gmail.com>
2024-06-25 12:21:33 -0400

    hi

--- myapp/views.py
+++ myapp/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.shortcuts import render
 
 from myapp.models import Book
+from django.http import HttpResponse
+from .forms import FeedbackForm
 
 
 def index(request):
@@ -15,3 +17,20 @@ def details(request, book_id):
 
 def about(request):
     return render(request, 'myapp/about.html')
+
+def getFeedback(request):
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            feedback = form.cleaned_data['feedback']
+            if feedback == 'B':
+                choice = ' to borrow books.'
+            elif feedback == 'P':
+                choice = ' to purchase books.'
+            else: choice = ' None.'
+            return render(request, 'myapp/fb_results.html', {'choice':choice})
+        else:
+            return HttpResponse('Invalid data')
+    else:
+        form = FeedbackForm()
+        return render(request, 'myapp/feedback.html', {'form':form})
