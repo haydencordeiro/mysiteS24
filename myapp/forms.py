2f0c9156f94b6c862c3c72d3743f07a7740185ff
Kishan Modi <kishandmodi@gmail.com>
2024-07-08 13:16:54 -0400

    FIx Issues Lab8

--- myapp/forms.py
+++ myapp/forms.py
@@ -2,34 +2,50 @@ from django import forms
 from django.forms.widgets import RadioSelect
 from .models import *
 
+
 class FeedbackForm(forms.Form):
     FEEDBACK_CHOICES = [
-        ('B', 'Borrow'),
-        ('P', 'Purchase'),
+        ("B", "Borrow"),
+        ("P", "Purchase"),
     ]
-    feedback = forms.ChoiceField(choices = FEEDBACK_CHOICES)
+    feedback = forms.MultipleChoiceField(
+        choices=FEEDBACK_CHOICES,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
 
 class SearchForm(forms.Form):
-    name = forms.CharField(required=False, label='Your Name')
+    name = forms.CharField(required=False, label="Your Name")
     category = forms.ChoiceField(
         required=False,
         choices=Book.CATEGORY_CHOICES,
-        label='Select a category:',
-        widget=RadioSelect
+        label="Select a category:",
+        widget=RadioSelect,
     )
-    max_price = forms.IntegerField(label='Maximum Price', min_value=0)
+    max_price = forms.IntegerField(label="Maximum Price", min_value=0)
 
 
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ['books', 'member', 'order_type']
-        widgets = {'books': forms.CheckboxSelectMultiple(), 'order_type':forms.RadioSelect}
-        labels = {'member': u'Member name', }
+        fields = ["books", "member", "order_type"]
+        widgets = {
+            "books": forms.CheckboxSelectMultiple(),
+            "order_type": forms.RadioSelect,
+        }
+        labels = {
+            "member": "Member name",
+        }
+
 
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ['reviewer', 'book', 'rating',  'comments']
-        widgets = {'books': forms.RadioSelect()}
-        labels = {'reviewer' : 'Please enter a valid email' , 'rating' : 'Rating: An integer between 1 (worst) and 5 (best)'}
+        fields = ["reviewer", "book", "rating", "comments"]
+        widgets = {
+            "book": forms.RadioSelect(),
+        }
+        labels = {
+            "reviewer": "Please enter a valid email",
+            "rating": "Rating: An integer between 1 (worst) and 5 (best)",
+        }
--- myapp/urls.py
+++ myapp/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from myapp import views
 
 
-app_name = 'myapp'
+app_name = "myapp"
 
 urlpatterns = [
-    path(r'', views.about, name='index'),
-    path(r'<int:book_id>', views.details, name='details'),
-    path(r'about/', views.about, name='about'),
-    path('feedback/', views.getFeedback, name='feedback1'),
-    path('findbooks/', views.findbooks, name='findbooks'),
-    path('place_order', views.place_order, name='place_order'),
-    path('review/', views.review_view, name='review')
+    path(r"", views.index, name="index"),
+    path(r"<int:book_id>", views.details, name="details"),
+    path(r"about/", views.about, name="about"),
+    path("feedback/", views.getFeedback, name="feedback1"),
+    path("findbooks/", views.findbooks, name="findbooks"),
+    path("place_order", views.place_order, name="place_order"),
+    path("review/", views.review, name="review"),
 ]
