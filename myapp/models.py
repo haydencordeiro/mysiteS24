e8eae67eb53d664d2177648c53d2015bfd3bedf4
Hayden Cordeiro <cordeirohayden@gmail.com>
2024-07-08 21:08:20 -0400

    Revert "FIx Issues Lab8"

--- myapp/models.py
+++ myapp/models.py
@@ -3,71 +3,64 @@ import datetime
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-
 # Create your models here.
 class Publisher(models.Model):
     name = models.CharField(max_length=200)
     website = models.URLField()
     city = models.CharField(max_length=20, blank=True)
-    country = models.CharField(max_length=50, default="USA")
-
+    country = models.CharField(max_length=50, default='USA')
 
 class Book(models.Model):
     CATEGORY_CHOICES = [
-        ("S", "Scinece&Tech"),
-        ("F", "Fiction"),
-        ("B", "Biography"),
-        ("T", "Travel"),
-        ("O", "Other"),
+        ('S', 'Scinece&Tech'),
+        ('F', 'Fiction'),
+        ('B', 'Biography'),
+        ('T', 'Travel'),
+        ('O', 'Other')
     ]
     title = models.CharField(max_length=200)
-    category = models.CharField(max_length=1, choices=CATEGORY_CHOICES, default="S")
+    category = models.CharField(max_length=1, choices=CATEGORY_CHOICES, default='S')
     num_pages = models.PositiveIntegerField(default=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    publisher = models.ForeignKey(
-        Publisher, related_name="books", on_delete=models.CASCADE
-    )
+    publisher = models.ForeignKey(Publisher, related_name='books', on_delete=models.CASCADE)
     description = models.TextField(blank=True, null=True)
-    num_reviews = models.PositiveIntegerField(default=0)
+    num_pages = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.title + " " + self.category + " " + self.get_category_display()
 
-
 # Lab4
 class Member(User):
     STATUS_CHOICES = [
-        (1, "Regular member"),
-        (2, "Premium Member"),
-        (3, "Guest Member"),
+        (1, 'Regular member'),
+        (2, 'Premium Member'),
+        (3, 'Guest Member'),
     ]
 
     status = models.IntegerField(choices=STATUS_CHOICES, default=1)
     address = models.CharField(max_length=300, blank=True, null=True)
-    city = models.CharField(max_length=20, default="Windsor")
-    province = models.CharField(max_length=2, default="ON")
+    city = models.CharField(max_length=20, default='Windsor')
+    province=models.CharField(max_length=2, default='ON')
     last_renewal = models.DateField(default=timezone.now)
     auto_renew = models.BooleanField(default=True)
     borrowed_books = models.ManyToManyField(Book, blank=True)
 
-
 class Order(models.Model):
     ORDER_VALUE_CHOICES = [
-        (0, "Purchase"),
-        (1, "Borrow"),
+        (0, 'Purchase'),
+        (1, 'Borrow'),
     ]
     books = models.ManyToManyField(Book)
-    member = models.ForeignKey(Member, related_name="orders", on_delete=models.CASCADE)
+    member = models.ForeignKey(Member, related_name='orders', on_delete=models.CASCADE)
     order_type = models.IntegerField(choices=ORDER_VALUE_CHOICES, default=1)
     order_date = models.DateField(default=timezone.now)
-
 
 class Review(models.Model):
     reviewer = models.EmailField()
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     rating = models.PositiveIntegerField()
-    comments = models.TextField(blank=True, null=True)
+    comments = models.TextField()
     date = models.DateField(default=timezone.now)
 
     def __str__(self):
-        return f"{self.reviewer} - {self.book.title} ({self.rating}/5)"
+        return self.comments
